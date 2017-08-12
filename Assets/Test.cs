7983d24e5543b1f2805b7ebf5669105ef1308bc2
Chambers <astring1812@126.com>
2017-08-13 00:34:46 +0800

    添加忽略文件，添加测试脚本，场景

---
+++ Assets/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    private float g;
+    void Start()
+    {
+        Debug.Log("Hello world");
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
